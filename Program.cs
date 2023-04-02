31b29973e010a8862812d1adf012553447549eae
wprestonneal <mrprestonneal@gmail.com>
2023-04-02 10:53:50 -0700

    function and if

--- Program.cs
+++ Program.cs
@@ -16,3 +16,23 @@ char c = 'C';
 Console.WriteLine($"Age is {age}");
 Console.WriteLine($"time alive is {timeAlive}");
 Console.WriteLine($"char is {c}");
+
+var something = 77;
+Console.WriteLine($"{something.GetType()}");
+
+
+bool IsEven(int i)
+{
+    return (i % 2 == 0);
+}
+
+var seconds = DateTime.Now.Second;
+
+if (IsEven(seconds))
+{
+    Console.WriteLine($"seconds {seconds} is even");
+}
+else 
+{
+    Console.WriteLine($"seconds {seconds} is odd");
+}

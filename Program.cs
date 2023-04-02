97771412a1b4c15752aae8e43a323a93e7470b3b
wprestonneal <mrprestonneal@gmail.com>
2023-04-02 11:18:49 -0700

    move to class

--- Program.cs
+++ Program.cs
@@ -1,52 +1,64 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
 
-string name = "old greg";
-Console.WriteLine(name);
-
-name = "new greg";
-Console.WriteLine(name);
-
-Console.WriteLine("Hello " + name);
-Console.WriteLine($"Hello interpolated {name}");
-
-int age = 25;
-double timeAlive = 20.6;
-char c = 'C';
-
-Console.WriteLine($"Age is {age}");
-Console.WriteLine($"time alive is {timeAlive}");
-Console.WriteLine($"char is {c}");
-
-var something = 77;
-Console.WriteLine($"{something.GetType()}");
-
-
-bool IsEven(int i)
+namespace funwithdotnet
 {
-    return (i % 2 == 0);
+    class FirstApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "old greg";
+            Console.WriteLine(name);
+
+            name = "new greg";
+            Console.WriteLine(name);
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine($"Hello interpolated {name}");
+
+            int age = 25;
+            double timeAlive = 20.6;
+            char c = 'C';
+
+            Console.WriteLine($"Age is {age}");
+            Console.WriteLine($"time alive is {timeAlive}");
+            Console.WriteLine($"char is {c}");
+
+            var something = 77;
+            Console.WriteLine($"{something.GetType()}");
+
+            var seconds = DateTime.Now.Second;
+
+            if (IsEven(seconds))
+            {
+                Console.WriteLine($"seconds {seconds} is even");
+            }
+            else
+            {
+                Console.WriteLine($"seconds {seconds} is odd");
+            }
+
+            string[] names = { "hello", "world" };
+
+            foreach (string n in names)
+            {
+                Console.WriteLine(n);
+            }
+
+            var words = new string[] { "give", "a", "little", "bit" };
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    
+        private static bool IsEven(int i)
+        {
+            return (i % 2 == 0);
+        }
+    }
+
 }
 
-var seconds = DateTime.Now.Second;
-
-if (IsEven(seconds))
-{
-    Console.WriteLine($"seconds {seconds} is even");
-}
-else 
-{
-    Console.WriteLine($"seconds {seconds} is odd");
-}
-
-string[] names = {"hello", "world"};
-
-foreach (string n in names)
-{
-    Console.WriteLine(n);
-}
-
-var words = new string[] {"give", "a", "little", "bit"};
-
-foreach (var word in words)
-{
-    Console.WriteLine(word);
-}

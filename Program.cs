3d58b4e18082827a9705b37a57a88cdd2421f492
wprestonneal <mrprestonneal@gmail.com>
2023-04-02 10:25:47 -0700

    numbers

--- Program.cs
+++ Program.cs
@@ -8,3 +8,11 @@ Console.WriteLine(name);
 
 Console.WriteLine("Hello " + name);
 Console.WriteLine($"Hello interpolated {name}");
+
+int age = 25;
+double timeAlive = 20.6;
+char c = 'C';
+
+Console.WriteLine($"Age is {age}");
+Console.WriteLine($"time alive is {timeAlive}");
+Console.WriteLine($"char is {c}");
